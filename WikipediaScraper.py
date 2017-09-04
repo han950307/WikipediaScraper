a407a4e90938f3c30ca43e7c66d1f8e17b7c6417
Hansung Kang <h.k@duke.edu>
2017-09-04 01:43:03 -0400

    added function to not cut words and wrap it smartly

--- WikipediaScraper.py
+++ WikipediaScraper.py
@@ -1,8 +1,94 @@
 import re
 import requests
 import urllib2
+import os
+import shlex
+import struct
+import platform
+import subprocess
 
 from pattern.web import Element
+
+
+class TerminalSizeGetter(object):
+    """
+    Helper class that gets terminal size. Source:
+    https://gist.githubusercontent.com/jtriley/1108174/raw/6ec4c846427120aa342912956c7f717b586f1ddb/terminalsize.py
+    """
+    def get_terminal_size(self):
+        """ getTerminalSize()
+         - get width and height of console
+         - works on linux,os x,windows,cygwin(windows)
+         originally retrieved from:
+         http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
+        """
+        current_os = platform.system()
+        tuple_xy = None
+        if current_os == 'Windows':
+            tuple_xy = self._get_terminal_size_windows()
+            if tuple_xy is None:
+                tuple_xy = self._get_terminal_size_tput()
+                # needed for window's python in cygwin's xterm!
+        if current_os in ['Linux', 'Darwin'] or current_os.startswith('CYGWIN'):
+            tuple_xy = self._get_terminal_size_linux()
+        if tuple_xy is None:
+            print "default"
+            tuple_xy = (80, 25)      # default value
+        return tuple_xy
+
+    def _get_terminal_size_windows(self):
+        try:
+            from ctypes import windll, create_string_buffer
+            # stdin handle is -10
+            # stdout handle is -11
+            # stderr handle is -12
+            h = windll.kernel32.GetStdHandle(-12)
+            csbi = create_string_buffer(22)
+            res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
+            if res:
+                (bufx, bufy, curx, cury, wattr,
+                 left, top, right, bottom,
+                 maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+                sizex = right - left + 1
+                sizey = bottom - top + 1
+                return sizex, sizey
+        except:
+            pass
+
+    def _get_terminal_size_tput(self):
+        # get terminal width
+        # src: http://stackoverflow.com/questions/263890/how-do-i-find-the-width-height-of-a-terminal-window
+        try:
+            cols = int(subprocess.check_call(shlex.split('tput cols')))
+            rows = int(subprocess.check_call(shlex.split('tput lines')))
+            return (cols, rows)
+        except:
+            pass
+
+    def _get_terminal_size_linux(self):
+        def ioctl_GWINSZ(fd):
+            try:
+                import fcntl
+                import termios
+                cr = struct.unpack('hh',
+                                   fcntl.ioctl(fd, termios.TIOCGWINSZ, '1234'))
+                return cr
+            except:
+                pass
+        cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+        if not cr:
+            try:
+                fd = os.open(os.ctermid(), os.O_RDONLY)
+                cr = ioctl_GWINSZ(fd)
+                os.close(fd)
+            except:
+                pass
+        if not cr:
+            try:
+                cr = (os.environ['LINES'], os.environ['COLUMNS'])
+            except:
+                return None
+        return int(cr[1]), int(cr[0])
 
 
 class Scraper(object):
@@ -44,6 +130,7 @@ class WikipediaScraper(Scraper):
     Scraper class for scraping basic information from wikipedia
     """
     GOOGLE_SEARCH_URL = "https://www.google.com/search?q={}"
+    CHAR_PER_LINE = 65
 
     contents = []
     cur_ind = 0
@@ -83,6 +170,32 @@ class WikipediaScraper(Scraper):
             print "No wikipedia summary found"
             return ""
 
+    def print_text(self, text):
+        """
+        prints the text so that it doesn't break char per line limit
+        or breaks words
+        """
+        tsg = TerminalSizeGetter()
+        sizex, sizey = tsg.get_terminal_size()
+        if not sizex:
+            sizex = self.CHAR_PER_LINE
+
+        words = text.split()
+        cur_ind = 0
+        buf = ""
+
+        # keep adding to buffer while ind is less than length
+        while cur_ind < len(words):
+            # if buf reached the char per line limit, then flush buffer
+            if len(buf) + len(words[cur_ind]) + 1 > sizex:
+                print buf.strip()
+                buf = ""
+
+            buf += words[cur_ind] + " "
+            cur_ind += 1
+
+        print buf.strip()
+
     def next(self):
         """
         prints the next paragraph from the wikipedia.
@@ -100,7 +213,7 @@ class WikipediaScraper(Scraper):
                 return
 
         if content_str:
-            print content_str
+            self.print_text(content_str)
 
     def search(self, token, hard_mode=False):
         """
